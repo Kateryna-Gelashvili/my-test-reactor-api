f19ce62b7a9e04b2a35e386b239613c5b351b852
katerynagelashvili <katerynag@11academianetworks.com>
2019-08-19 11:03:56 +0200

    added docker comment

--- src/main/java/com/k/myreactorapi/config/R2dbcConfig.java
+++ src/main/java/com/k/myreactorapi/config/R2dbcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static io.r2dbc.spi.ConnectionFactoryOptions.*;
-
+//docker run --name postgres -e POSTGRES_PASSWORD=root -e POSTGRES_USER=root -p 5432:5432 -d postgres:11
 @Configuration public class R2dbcConfig {
   @Bean public ConnectionFactory connectionFactory() {
     ConnectionFactoryOptions connectionFactoryOptions = ConnectionFactoryOptions.builder()
